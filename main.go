8d61912fc4b0145c616d2ec9085ec52d43f63d76
dataKingToday <tmddn1026@naver.com>
2022-02-17 01:51:22 +0900

    first commit

--- main.go
+++ main.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Println("hello world 2")
 	fmt.Println("hello world 3")
 	fmt.Println("hello world 4")
+	fmt.Println("hello world 5")
+	fmt.Println("hello world 6")
+	fmt.Println("hello world 7")
+	fmt.Println("hello world 8")
 }
